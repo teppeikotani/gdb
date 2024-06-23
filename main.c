d033ba896066fc3d344c9484fe91333da8949406
Teppei Kotani <cnj14052@ict.nitech.ac.jp>
2024-06-23 14:43:24 +0900

    change code

--- main.c
+++ main.c
@@ -1,25 +1,40 @@
 #include <stdio.h>
 
-int myfunc(int x, int y)
+// 関数プロトタイプの宣言
+void findMaxMin(int arr[], int n, int *max, int *min);
+
+int main()
 {
-    int z;
+    int i;
+    int arr[10] = {5, 3, 1, 6, 9, 8, 2, 6, 0, 7};
 
-    z = 2 * x - y;
-    return z;
-}
+    int max, min;
 
-int main(void)
-{
-    int a[5] = {1, 2, 3, 4, 5};
-    int b, c;
+    // 最大値と最小値を見つける関数を呼び出す
+    findMaxMin(arr, 10, &max, &min);
 
-    c = a[1] + a[5];
-
-    b = c + a[1];
-
-    b = myfunc(b, c);
-
-    printf("b=%d\n", b);
+    // 結果を出力
+    printf("最大値: %d\n", max);
+    printf("最小値: %d\n", min);
 
     return 0;
+}
+
+// 配列の最大値と最小値を見つける関数
+void findMaxMin(int arr[], int n, int *max, int *min)
+{
+    *max = arr[0];
+    *min = arr[0];
+
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > *max)
+        {
+            *max = arr[i];
+        }
+        if (arr[i] < *min)
+        {
+            *min = arr[i];
+        }
+    }
 }
